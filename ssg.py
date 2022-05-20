82bbbd8d32c49899482a92b686b9462dfc735976
kallen0576 <35823319+kallen0576@users.noreply.github.com>
2022-05-20 16:30:12 -0400

    Completed module

--- ssg.py
+++ ssg.py
@@ -7,6 +7,6 @@ from ssg.site import Site
 #if __name__ == "___main__"():
 def main(source = "content",dest = "dist"):
         config = {"source": "source",'dest': 'dist'}
-        Site(**config.build)
+        Site(**config).build()
 
 typer.run(main)
